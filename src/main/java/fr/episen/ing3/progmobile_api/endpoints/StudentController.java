64f9f2482d6b51ff4e84b314e335652c8ef2c5e4
PiJacquet <44034432+PiJacquet@users.noreply.github.com>
2020-10-14 16:33:34 +0200

    Fixing API

--- src/main/java/fr/episen/ing3/progmobile_api/endpoints/StudentController.java
+++ src/main/java/fr/episen/ing3/progmobile_api/endpoints/StudentController.java
@@ -2,8 +2,7 @@ package fr.episen.ing3.progmobile_api.endpoints;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,25 +23,33 @@ public class StudentController {
 
 	StudentService studentService;
 	
+	public StudentController() {
+		studentService = new StudentService();
+	}
+	
 	@PostMapping("/student")
 	/* Créer un étudiant */
-	public void createStudent(@RequestBody Student student) {
+	public ResponseEntity<Student> createStudent(@RequestBody Student student) {
 		log.info("create " + student.toString());
 		Student newStudent = studentService.createStudent(student);
+		return ResponseEntity.ok(newStudent);
 	}
 	
 	@PutMapping("/student")
 	/* Met à jour un étudiant */
-	public void updateStudent(@PathParam("student") Student student) {
+	public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
 		log.info("update " + student.toString());
 		Student updateStudent = studentService.updateStudent(student);
+		return ResponseEntity.ok(updateStudent);
 	}
 	
+	@SuppressWarnings("rawtypes")
 	@DeleteMapping("/student/{id}")
 	/* Supprime un étudiant */
-	public void deleteStudent(@PathParam("id") Integer id) {
+	public ResponseEntity deleteStudent(@PathVariable("id") Integer id) {
 		log.info("delete " + id);
 		Boolean isDeleted = studentService.deleteStudent(id);
+		return ResponseEntity.ok().build();
 	}
 	
 	@GetMapping("/student/{id}")
